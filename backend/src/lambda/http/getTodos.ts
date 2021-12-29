348d123ff19a08e3d425699302e13fb23fcb5748
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-12-29 07:57:21 -0600

    gets all todos for Ghost Rider

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,22 +1,38 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-// import * as middy from 'middy'
-// import { cors } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
-// import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-// import { getUserId } from '../utils';
 
-// // TODO: Get all TODO items for a current user
-// export const handler = middy(
-//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//     // Write your code here
-//     const todos = '...'
+const logger = createLogger('GetTodos')
 
-//     return undefined
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Getting all the todos for Ghost Rider ', event)
+    const todos = await new DocumentClient().query({
+        TableName: process.env.TODO_TABLE_NAME,
+        ExpressionAttributeValues: {
+          ':userId': 'Ghost Rider'
+        },
+        KeyConditionExpression: 'userId = :userId'
+    }).promise()
 
-// handler.use(
-//   cors({
-//     credentials: true
-//   })
-// )
+    logger.info(`Found these items for Ghost Rider: ${JSON.stringify(todos.Items)}`)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({items: todos.Items})
+    }
+  })
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
