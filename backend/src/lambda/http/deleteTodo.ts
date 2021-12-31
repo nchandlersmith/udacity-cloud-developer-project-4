d454496209e487f3decbb8e27a4fb8145853e7e3
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-12-31 13:49:09 -0600

    adds delete endpoint

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,25 +1,50 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-// import * as middy from 'middy'
-// import { cors, httpErrorHandler } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+
 
 // import { deleteTodo } from '../../businessLogic/todos'
 // import { getUserId } from '../utils'
 
-// export const handler = middy(
-//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//     const todoId = event.pathParameters.todoId
-//     // TODO: Remove a TODO item by id
-    
-//     return undefined
-//   }
-// )
+const logger = createLogger('DeleteTodo')
+const table = process.env.TODO_TABLE_NAME
 
-// handler
-//   .use(httpErrorHandler())
-//   .use(
-//     cors({
-//       credentials: true
-//     })
-//   )
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    // TODO: Remove a TODO item by id
+    logger.info(`Attempting to delete todo with id: ${todoId} from table: `)
+
+    await new DocumentClient().delete({
+        TableName: table,
+        Key: {
+            "todoId": todoId,
+            "userId": "Ghost Rider"
+        }
+    }).promise().catch(error => {
+        const message = `Error encountered while deleting ${todoId}. Error: ${JSON.stringify(error)}`
+        console.error(message)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message})
+        }
+    })
+
+    return {
+        statusCode: 204,
+        body: JSON.stringify({event: `${event}`})
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
