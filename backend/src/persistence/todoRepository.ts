02c91e0001099e784b35000ad4ccdce5c19a6f89
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-01-02 15:58:24 -0600

    refactor table name bubbles to top

--- backend/src/persistence/todoRepository.ts
+++ backend/src/persistence/todoRepository.ts
@@ -3,12 +3,12 @@ import { TodoItem } from "../models/TodoItem"
 import { createLogger } from "../utils/logger"
 
 const logger = createLogger('Todo Repository')
-
+const tableName = process.env.TODO_TABLE_NAME
 
 export async function insertTodo(todo: TodoItem): Promise<TodoItem> {
     logger.info('Inserting todo.')
     await new DocumentClient().put({
-      TableName: process.env.TODO_TABLE_NAME,
+      TableName: tableName,
       Item: todo
     }).promise()
     return todo
@@ -17,7 +17,7 @@ export async function insertTodo(todo: TodoItem): Promise<TodoItem> {
   export async function findAllTodosByUser(userId: string): Promise<DocumentClient.QueryOutput> {
     logger.info('Finding todos by user.')
     return new DocumentClient().query({
-      TableName: process.env.TODO_TABLE_NAME,
+      TableName: tableName,
       ExpressionAttributeValues: {
         ':userId': userId
       },
