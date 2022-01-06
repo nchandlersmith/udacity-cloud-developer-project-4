1863470f9c3364ae36102ea0dd9a1d1c8a3ccd33
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-01-05 21:50:06 -0600

    removes logs that really should not be there

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,16 +9,12 @@ import axios from 'axios'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 const jwksUrl = 'https://dev-or580l07.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
-  logger.info(`Actual log of auth header: ${event.authorizationToken}`)
+  logger.info(`Authorizing a user: ${event.authorizationToken}`)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
@@ -56,22 +52,16 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
+  logger.info('Verifying token...')
   const token = getToken(authHeader)
-  logger.info(`token: ${token}`)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
   const response = await axios.get(jwksUrl)
   const jwk = response.data
-  logger.info(`jwt: ${JSON.stringify(jwt)}`)
-  logger.info(`jwk: ${JSON.stringify(jwk)}`)
   const x5c = jwk.keys.find(key => key.kty === 'RSA' && key.kid === jwt.header.kid).x5c[0]
   const prefix = '-----BEGIN CERTIFICATE-----\n'
   const suffix = '\n-----END CERTIFICATE-----'
   const cert = `${prefix}${x5c}${suffix}`
-  logger.info(`cert: ${cert}`)
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
   const check = verify(token, cert, { algorithms: [ 'RS256' ]}) as JwtPayload
   if (check) {
     logger.info('Checks pass!')
